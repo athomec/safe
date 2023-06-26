a2ee7588bc743538287ff1dc3acaf2abc608f0f0
athomec <athomec@pie.com.tw>
2023-06-26 10:22:42 +0800

    上傳新頁面

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,36 @@ $(function () {//JS開頭
 		}
 	});
 
-	var backdrop = null;
-
-	$('.js-nav-toggler').click(function () {
-		if (backdrop === null) {
-			backdrop = $('<div class="modal-backdrop custom-backdrop"></div>');
-			$('body').append(backdrop);
-			backdrop.fadeIn();
-		} else {
-			backdrop.fadeToggle();
-		}
+	$(".js-toggler").click(function () {
+		$(this).toggleClass("active");
+	})
+	$(".js-send").click(function () {
+		$(this).addClass("active");
+		var countdown = 297; // 倒數秒數的初始值
+	
+		// 將按鈕設置為禁用狀態，避免重複點擊
+		$(this).prop('disabled', true);
+	
+		// 更新按鈕文字為倒數秒數
+		$(this).html(countdown + " 秒後可重新發送");
+	
+		// 啟動計時器，每過一秒執行一次
+		var timer = setInterval(function () {
+			countdown--; // 減少秒數
+	
+			// 更新按鈕文字為倒數秒數
+			$(this).html(countdown + " 秒後可重新發送");
+	
+			// 如果倒數結束，清除計時器並還原按鈕狀態
+			if (countdown <= 0) {
+				clearInterval(timer); // 清除計時器
+				$(this).prop('disabled', false); // 還原按鈕狀態
+				$(this).html("重新發送驗證碼"); // 還原按鈕文字
+				$(this).removeClass("active");
+			}
+		}.bind(this), 1000); // 設置計時器間隔為1秒(1000毫秒)
 	});
+
 
 	RESIZE();
 
